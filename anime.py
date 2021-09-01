47b20db9412d8052b2c2a4061b64fcef75f2c4d6
rgps4502 <55177355+rgps4502@users.noreply.github.com>
2021-09-02 02:57:06 +0800

    動漫更新通知
    動漫更新通知

--- anime.py
+++ anime.py
@@ -21,6 +21,7 @@ def data():
     title = soup.find("div", id='portal_block_951_content')  # 尋找新翻的div,id
     titles = title.find(["style='width: 180px;'", 'ul'])  # 尋找div裡面的元素
     item = []
+    item.insert(0, '')
     for name in titles:
         if name != None:
             # 取得<a>標籤的連結文字getText()方法(Method) 標籤底下所有<p>標籤select_one()方法進行選取
@@ -28,28 +29,31 @@ def data():
     return item
 
 
+def sendmessage(message):
+    headers = {
+        "Authorization": "Bearer " + "KZuA1PCElIXss7soRvLTTr4Y3m5GSXjSyjye0gSMhFv",
+        "Content-Type": "application/x-www-form-urlencoded"}
+    params = {"message": message}
+    r = requests.post("https://notify-api.line.me/api/notify",
+                      headers=headers, params=params)
+    print(r.status_code)  # 200
+
+
 url = 'https://myself-bbs.com/portal.php'
 oldDate = data()
-print('\n'.join(map(str, oldDate)))  # 打印出換行的列表
+message = ('\n'.join(map(str, oldDate)))  # 打印出換行的列表
+sendmessage(message)
 while(True):
     while(True):
-        time.sleep(300)
+        time.sleep(8)
         newDate = data()
         if newDate != oldDate:
             # 比較打印出不同的新翻 ^不同 &相同
             Compared = (list(set(newDate) ^ set(oldDate)))
-            Compared.insert(1, '')
-            newUpdate = ('\n'.join(map(str, Compared)))
-            del(newUpdate[0])  # 移除列表第一個選項
-            #kzuaIP這串是Line的token
-            headers = {
-                "Authorization": "Bearer " + "KZuA1PCElIXss7soRvLTTr4Y3m5GSXjSyjye0gSMhFv",
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-            params = {"message": newUpdate}
-            r = requests.post("https://notify-api.line.me/api/notify",
-                              headers=headers, params=params)
-            print(r.status_code)  # 200
+            Compared.insert(0, '')
+            Compared.pop(1)  # 移除列表最後一個選項
+            message = ('\n'.join(map(str, Compared)))
+            sendmessage(message)
             oldDate = data()
             break
         print('這5分鐘沒有更新')
