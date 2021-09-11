ef0cb8d055d03343ac9443b8dfa0faea303d92e6
rgps4502 <rgps45021@gmail.com>
2021-09-11 10:53:38 +0800

    動漫更新通知
    動漫更新通知

--- anime.py
+++ anime.py
@@ -1,4 +1,4 @@
-#* -*- coding: utf-8 -*-
+# * -*- coding: utf-8 -*-
 from typing import NewType, Text
 import requests
 import time
@@ -13,8 +13,29 @@ def get_http(url):
         r.encoding = r.apparent_encoding  # 用預測的解碼是正確的
         return r.text  # 顯示網頁內容
     except:
-        print('連線失敗')
-        return "發生異常"
+        print('1.連線失敗一分後重新嘗試')
+        time.sleep(60)
+        session = requests.session()
+        r = session.get(url, timeout=30)  # 獲取網頁30秒為限
+        r.raise_for_status()  # 如果狀態不是200引發HTTPERROR異常
+        r.encoding = r.apparent_encoding  # 用預測的解碼是正確的
+        return r.text  # 顯示網頁內容
+    except:
+        print('2.連線失敗一分後重新嘗試')
+        time.sleep(60)
+        session = requests.session()
+        r = session.get(url, timeout=30)  # 獲取網頁30秒為限
+        r.raise_for_status()  # 如果狀態不是200引發HTTPERROR異常
+        r.encoding = r.apparent_encoding  # 用預測的解碼是正確的
+        return r.text  # 顯示網頁內容
+    except:
+        print('3.連線失敗一分後重新嘗試')
+        time.sleep(60)
+        session = requests.session()
+        r = session.get(url, timeout=30)  # 獲取網頁30秒為限
+        r.raise_for_status()  # 如果狀態不是200引發HTTPERROR異常
+        r.encoding = r.apparent_encoding  # 用預測的解碼是正確的
+        return r.text  # 顯示網頁內容
 
 
 def data():
@@ -29,6 +50,8 @@ def data():
             # 取得<a>標籤的連結文字getText()方法(Method) 標籤底下所有<p>標籤select_one()方法進行選取
             item += [(name.select_one('p').getText())]
     return item
+
+
 def sendmessage(message):
     headers = {
         "Authorization": "Bearer " + "KZuA1PCElIXss7soRvLTTr4Y3m5GSXjSyjye0gSMhFv",
@@ -49,7 +72,8 @@ while(True):
         time.sleep(300)
         newDate = data()
         if newDate != oldDate:
-            Compareddf=[y for y in (oldDate+newDate) if y not in all_list] #比較打印出在總表list不相同的
+            # 比較打印出在總表list不相同的
+            Compareddf = [y for y in (oldDate+newDate) if y not in all_list]
             for newan in Compareddf:
                 all_list.insert(1, newan)  # 把新的插入總表做後續比較用
                 all_list.pop()  # 移除總表的最後一個保持15個
