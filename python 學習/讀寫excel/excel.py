7ba3f94ad5bc5d86dfe3317c6d2f4a5fd16d8fe3
rgps4502 <rgps45021@gmail.com>
2021-10-06 00:54:05 +0800

    excel
    excel

--- python 學習/讀寫excel/excel.py
+++ python 學習/讀寫excel/excel.py
@@ -9,3 +9,5 @@ for i in df['产品']:
     if i == 'A02':
         df.loc[df['产品'] == 'A02'].to_excel(i+'.xlsx', sheet_name=i)
         # print(df.loc[df['产品'] == 'A02'])
+
+# 參考網址 https://blog.csdn.net/weixin_43245453/article/details/90747259
