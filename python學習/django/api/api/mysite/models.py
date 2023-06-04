ab2c432e9deac2806c4a067006c98534deb658e2
rgps45022 <rgps45022@gmail.com>
2023-06-05 00:30:05 +0800

    django操作mysql將數據顯示到前端

--- python學習/django/api/api/mysite/models.py
+++ python學習/django/api/api/mysite/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class fourm_post(models.Model):
+    title = models.TextField(null=True,verbose_name="主題")
+    content = models.TextField(null=True,verbose_name="內容")
+    user_name = models.CharField(max_length=100,default="用戶不存在",verbose_name="用戶名")
+    fourm_name = models.CharField(max_length=100,default="把名不存在",verbose_name="把名")
+    publish_time = models.DateTimeField(verbose_name="發帖時間",default=datetime.now())
+
+
+class fourm_search(models.Model):
+    title = models.TextField(null=True,verbose_name="主題")
+    content = models.TextField(null=True,verbose_name="內容")
+    title_id = models.BigIntegerField(verbose_name="主題ID")
+    content_id = models.BigIntegerField(verbose_name="內容ID")
+    fourm_name = models.CharField(max_length=100,default="把名不存在",verbose_name="把名")
+    publish_time = models.DateTimeField(verbose_name="發帖時間",default=datetime.now())
+    choice_result = models.TextField(verbose_name="標籤")
+    sign = models.BigIntegerField(verbose_name="標記",default="0")
--- python學習/django/api/api/mysite/views.py
+++ python學習/django/api/api/mysite/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 
-from django.http import HttpResponse
-
 def home_page(request):
-    return render(request,"mysite/home.html",{"result":"abc.com","name":'jea'})
+    post = models.fourm_post.objects.get(pk=3)
+    return render(request,"mysite/home.html",{"post":post})
